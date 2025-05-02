a1ba6058ed6246f8a53e920b26650e562669951b
Valentin Leon Stöcker <valentin@stoeckermail.de>
2025-05-02 18:41:14 +0200

    more try

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
         };
 
         hydraJobs = {
-          blobstore = self.x86_64-linux.packages.x86_64-linux.blobstore;
+          blobstore = self.x86_64-linux.packages.blobstore;
         };
       }
     ));
